2c2378300a437145b81416968658a5c5485446f8
Hrishikesh Sawant <hrishikeshsawant@hotmail.com>
2012-05-19 01:17:21 +0530

    keydown changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,42 +3,68 @@
 $(function(){
 
 	var socket = io.connect('http://localhost:8000');
+	var maxLimitReached = false;
 
 	$('#infoBox').hide();
 
 	socket.on('chatMessage', function(data){
 		var messageLength = data.message.length;
 
-		if(messageLength > 0 && messageLength < 10){
+		if(messageLength > 0 && messageLength < 50){
 			$('#messageList').prepend($('<li>',{ 'text':data.message, class:'message'}));
 		}
 	});
 
-	$('#mainForm').on('submit', function(e) { 
+	function formSubmit(e) { 
+		console.log('sadsad');
 		e.preventDefault();
 		var $inputBox = $('#messagetxt');
 		socket.emit('chatMessage', {'message':$inputBox.val()});
 
 		$inputBox.val('');
-	});
+	}
+
+	$('#mainForm').on('submit', formSubmit);
+
+	
 
 	$('#messagetxt').on('keydown', function(e){
 		var messageLength = $(this).val().length;
-		if(messageLength > 10){
-			
+		if(messageLength == 50){
+			maxLimitReached = true;
 			$('#infoBox').text('Max character limit reached : 50');
 			$('#infoBox').slideDown();
 			$('#infoBox').on('click',function(){
 				$(this).slideUp();
 			});
-			
+			$('#mainForm').off();
 		}
 		else{
+			maxLimitReached = false;
 			$('#infoBox').slideUp();
+			$('#mainForm').on('submit', formSubmit);
+		}
 
+	});
+
+	$('#submitbtn').on('click', function(){
+		if(maxLimitReached){
+			return false;
 		}
 
 	});
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
 
